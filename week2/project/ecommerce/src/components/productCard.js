79584380249f4d14b717f05873b31eebcec93365
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-06-07 13:45:55 +0200

    created link to detail page

--- week2/project/ecommerce/src/components/productCard.js
+++ week2/project/ecommerce/src/components/productCard.js
@@ -1,22 +1,29 @@
-import './productCard.css';
+import './ProductCard.css';
+import {Link} from 'react-router-dom';
 
-function ProductCard({product}) {
+function ProductCard({product, setCategory}) {
   const {
+    id,
     image,
     title,
-    category,
+    cat,
     rating: {rate, count},
     price,
   } = product;
   return (
     <div className="col-md-6 col-lg-4 col-xl-4">
-      <div className="card overflow-hidden"  style={{marginBottom: "15px", padding: "10px", height: "30rem"}}>
+      <div
+        className="card overflow-hidden"
+        style={{marginBottom: '15px', padding: '10px', height: '30rem'}}
+      >
         <div className="image">
-          <img src={image} alt="product"/>
+          <img src={image} alt="product" />
         </div>
         <div className="text mt-2">
-          <h5>{title}</h5>
-          <h5>{category}</h5>
+          <Link to={`/product/${id}`} onClick={() => setCategory('')}>
+            <h5 className='text-dark title-link'>{title}</h5>
+          </Link>
+          <h5>{cat}</h5>
           <div className="rating_reviews mt-3">
             <p>Rating: {rate}</p>
             <p>{count} reviews</p>
