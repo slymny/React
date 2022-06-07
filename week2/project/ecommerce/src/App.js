87643cf62dd933c56d055d1132a45561423deaff
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-06-07 13:44:47 +0200

    id router is added

--- week2/project/ecommerce/src/App.js
+++ week2/project/ecommerce/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ProductSection from './pages/productPage';
+import ProductPage from './pages/ProductPage';
 import {useState, useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Layout from './layout/Layout';
@@ -17,17 +17,17 @@ function App() {
       setAllCategories={setAllCategories}
     >
       <Routes>
-        <Route path="/*" element={<ProductSection category="" />} />
+        <Route path="/*" element={<ProductPage category="" />} />
         {allCategories.map(category => {
           return (
-            <Route key={category}
+            <Route
+              key={category}
               path={`/${category.split(' ').join('%20')}`}
-              element={<ProductSection category={category} />}
-            >
-              <Route path=":id" element={<DetailPage category={category} />} />
-            </Route>
+              element={<ProductPage category={category} setCategory={setCategory} />}
+            />
           );
         })}
+        <Route path={`product/:id`} element={<DetailPage />} />
       </Routes>
     </Layout>
   );
