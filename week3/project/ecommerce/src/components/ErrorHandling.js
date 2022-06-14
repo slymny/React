20b8b2cd70f14eaee6929e80023c71e6a114e1f1
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-06-15 00:04:15 +0200

    Unchanged files

---
+++ week3/project/ecommerce/src/components/ErrorHandling.js
@@ -0,0 +1,10 @@
+
+function ErrorHandling({message}) {
+  return (
+    <div className="container text-danger text-center mt-5">
+      <h3>{message}</h3>
+    </div>
+  )
+}
+
+export default ErrorHandling
